fc34f244266d197a7ba7c93e5cebfbcb359b3f18
hitechvenkadesh <48478948+hitechvenkadesh@users.noreply.github.com>
2022-04-23 08:43:22 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
   type = string
 }
-variable "secret_access_key" {
+variable "secret_key" {
   type = string
 }
 
 provider "aws" {
   region = "ap-south-1"
   access_key = var.access_key
-  secret_key = var.secret_access_key
+  secret_key = var.secret_key
   }
 
 resource "aws_iam_user" "sowmi"{
